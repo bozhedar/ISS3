844bab526bd82bee1f01e58c4209abb5972f7d9d
bogda <bogdanskrip404@gmail.com>
2025-11-25 01:14:33 +0500

    init

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+import org.example.util.TotalSumCounterUtil;
+
+public class Main {
+    public static void main(String[] args) {
+        TotalSumCounterUtil.saveInTextFile();
+        }
+    }
